f25393a3ae00778a88617ee2a2d96b1beb984c60
J Cole Morrison <j.cole.morrison@gmail.com>
2014-04-15 18:37:16 -0700

    The Yeoman project at the end of part 1

---
+++ server/app.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+/// error handlers
+
+/**
+ * Development Settings
+ */
+if (app.get('env') === 'development') {
+	// This will change in production since we'll be using the dist folder
+	// This covers serving up the index page
+	app.use(express.static(path.join(__dirname, '../client/.tmp')));
+	app.use(express.static(path.join(__dirname, '../client/app')));
+
+	// Error Handling
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+/**
+ * Production Settings
+ */
+if (app.get('env') === 'production') {
+
+	// changes it to use the optimized version for production
+	app.use(express.static(path.join(__dirname, '/dist')));
+
+	// production error handler
+	// no stacktraces leaked to user
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: {}
+		});
+	});
+}
+
+
+module.exports = app;
