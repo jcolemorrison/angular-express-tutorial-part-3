bd0746c4afe61e0ec4dede2019602169512672bf
J Cole Morrison <j.cole.morrison@gmail.com>
2014-04-16 01:55:43 -0700

    Removed Jade from our dependencies

--- server/app.js
+++ server/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 app.use(favicon());
